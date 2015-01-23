430e5fda6e1473e476d5e4612b09afefdbd9a149
chiayolin <chiayo.lin@gmail.com>
2015-01-23 07:11:22 +0700

    improved something

--- asdc/asdc.c
+++ asdc/asdc.c
@@ -2,57 +2,51 @@
  * Description:
  * ASDC - Average and Standard Deviation Calculator
  * 
- * Author: Chiayo Lin
- * Email : chiayo.lin@gmail.com
- * Date  : Sep 04, 2014
- * Site  : www.chiayolin.org
+ * Author : Chiayo Lin
+ * Email  : chiayo.lin@gmail.com
+ * Date   : Sep 04, 2014
+ * License: GPL 3.0
  *
  */
 
 #include <stdio.h>
 #include <math.h>
-#define MAX 1000
 
-float square(float b);
+#define MAX 100
+
+float square(float b) { 
+	return b * b;
+}
 
 int main() {
-	float input[MAX] = { 0 };
-	int len, index = 1;
+	float input[MAX] = {};
 	float sum, avg, sd;
+	int len, index = 1;
 	sd = avg = len = sum = 0;
 
-	printf("Lenght            : ");
+	printf("lenght            : ");
 	scanf("%d", &len);
 
 	while(index <= len) {
-		printf("Input[%02d]         : ", index);
+		printf("input[%02d]         : ", index);
 		scanf("%f", &input[index]);
 		sum = input[index] + sum;
-		++index;
-	}
-
+	
+	/* Get average */
 	avg = sum / len;
-	sum = 0;
-	index = 1;
-
+	sum = 0, index = 1;
+	
+	/* Get standard deviation */
 	int i;
 	for(i = 0; i < len; i++) {
 		input[index] = input[index] - avg;
 		sum = sum + square(input[index]);
 		++index;
 	} 
-
 	sd = sqrt(sum / len);
 	
-	printf("\nAverage           : %.3f\n", avg);
-	printf("Standard Deviation: %.3f\n", sd);
+	printf("\naverage           : %.3f\n", avg);
+	printf("standard deviation: %.3f\n", sd);
 
 	return 0;
-}
-
-float square(float b) {
-	float s;
-	s = b * b;
-
-	return s;
 }
