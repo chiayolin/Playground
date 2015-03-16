ccc71510a4d433f071e4ab3adb99dbc87c97a62b
Chiayo Lin <chiayo.lin@gmail.com>
2015-03-16 21:15:18 +0700

    Update getch.h

--- getch/getch.h
+++ getch/getch.h
@@ -1,5 +1,5 @@
 /*
- * getch.h - a implementation of getch() and getche() 
+ * getch.h - an implementation of getch() and getche() 
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
