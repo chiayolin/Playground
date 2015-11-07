4a83fa51d28ecd6e013bdf45b38c700a2c5dba1e
chiayolin <chiayo.lin@gmail.com>
2015-11-07 22:02:31 +0700

    first revision

--- learn-python/CH4_D1.py
+++ learn-python/CH4_D1.py
@@ -1,37 +1,39 @@
-#This program will allow the user to type a message and have it converted into
-#Morse code
-code = [('A',".-"),   ('B',"-..."), ('C',"-.-."), ('D',"-.."), ('E',"."),\
-           ('F',"..-."), ('G',"--."),  ('H',"...."), ('I',".."),  ('J',".---"),\
-           ('K',"-.-"),  ('L',".-.."), ('M',"--"),   ('N',"-."),  ('O',"---"),\
-           ('P',".--."), ('Q',"--.-"), ('R',".-."),  ('S',"..."), ('T',"-"),\
-           ('U',"..-"),  ('V',"...-"), ('W',".--"),  ('X',"-..-"),('Y',"-.--"),\
-           ('Z',"--..")]
-print('This program will convert sentences to Morse Code.')
-print('Sentences may only contain letters, spaces or periods.')
-print()
-correct_item = False
-terminate = False
-while not correct_item:
-    item = input('Enter your sentence (words and spaces only. Type in capitals)): ')
-    
-    string = input().upper()
+## CH4_D1.py - Morsecode converter 
+# Authors: Nirat Singh, Chiayo Lin  
+# License: GPL 3.0
 
-for ch in item:
-    found_ch= False
-    while not found_ch:
-        counter = 0
-        if ch == letter[counter][0]:
-            print(letter[counter][1])
-            found_ch= True
-    for letter in string:
-        print("%s " % (code[code.index(letter) + 1] \
-        if letter in code else letter), end=' ')
-print(end="\n")
-while terminate:
-    repeat = input('Again? (y or n): ')
-    if 'n' or 'N':
-        terminate = True
+## init 
+code = [('A',".-"), ('B',"-..."), ('C',"-.-."), ('D',"-.."), ('E',"."), 
+    ('F',"..-."), ('G',"--."),  ('H',"...."), ('I',".."),  ('J',".---"),
+    ('K',"-.-"),  ('L',".-.."), ('M',"--"),   ('N',"-."),  ('O',"---"),
+    ('P',".--."), ('Q',"--.-"), ('R',".-."),  ('S',"..."), ('T',"-"),
+    ('U',"..-"),  ('V',"...-"), ('W',".--"),  ('X',"-..-"),('Y',"-.--"),
+    ('Z',"--..")]
+
+## info
+print('this is a plaintext to Morsecode converter,\n' + \
+    'input may contain letters only.')
 
 
+## loop
+while True:
+    ## prompt and get input
+    string = input('enter your sentence: ').upper().rstrip()
 
+    ## main and error handling 
+    try:
+        for ch in string:
+            counter = 0
+            found_item = False
+            while not found_item:
+                if ch == code[counter][0]:
+                    print(code[counter][1], end='')
+                    found_item = True
+                counter += 1
+    except IndexError:
+        print("error: invalid character(s)", end='')
+    print(end='\n')
 
+    ## terminate?
+    quit() if input('again? (y/n): ').split()[0][0].lower() != 'y' \
+        else None
