1b4f5a2f30c2844215fb6d0084543fbedf120280
chiayolin <chiayo.lin@gmail.com>
2017-05-01 22:43:23 +0700

    add chaining method

---
+++ learn-java/Chaining.java
@@ -0,0 +1,38 @@
+class Person {
+    private String firstName; 
+    private String lastName;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFullName() {
+        /* `return this.firstName + " " + this.lastName;` */
+        /* without syntactic sugar */
+        return this.firstName.concat(" ").concat(this.lastName);
+    }
+
+    public Person(String firstName, String lastName) {
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+}
+
+public class Chaining {
+    public static void main(String args[]) {
+        Person dylan = new Person("Bob", "Dylan");
+        System.out.println(dylan.getFullName());
+    }
+}
