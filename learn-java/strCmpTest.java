3c37372914fdf0a5b7af89d71b636dd0c8f8285d
chiayolin <chiayo.lin@gmail.com>
2017-05-01 23:18:22 +0700

    minor update

--- learn-java/strCmpTest.java
+++ learn-java/strCmpTest.java
@@ -4,9 +4,9 @@ class strCmpTest {
         String str2 = new String("hello, world");
         
         /* compares the object "references" */
-        System.out.println(str1 == str2);
+        System.out.println(str1 == str2); // false
 
         /* compares the object "values" */
-        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str2)); // true
     }
 }
