d4f4ca3134c248d079aacb3e7ce146334dd91c64
chiayolin <chiayo.lin@gmail.com>
2017-05-01 23:17:40 +0700

    difference between == and .equals

---
+++ learn-java/strCmpTest.java
@@ -0,0 +1,12 @@
+class strCmpTest {
+    public static void main(String atg[]) {
+        String str1 = new String("hello, world");
+        String str2 = new String("hello, world");
+        
+        /* compares the object "references" */
+        System.out.println(str1 == str2);
+
+        /* compares the object "values" */
+        System.out.println(str1.equals(str2));
+    }
+}
